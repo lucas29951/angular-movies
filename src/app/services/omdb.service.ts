29df016c0d99665ec680631ce196ec4d0a5afc1b
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-15 17:07:02 -0300

    Configuracion de la funcion para buscar peliculas mediante la API

--- src/app/services/omdb.service.ts
+++ src/app/services/omdb.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OmdbService {
+  private apiKey: string = '';
+  private baseUrl: string = 'http://www.omdbapi.com/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  searchMovies(query: string): Observable<any> {
+    const url = `${this.baseUrl}?s=${query}&apiKey=${this.apiKey}`;
+    return this.http.get(url);
+  }
 }
