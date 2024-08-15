ee0ad249c8db6f897fbcc7556c18388857f19358
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-15 17:23:55 -0300

    Logica implementada para la busqueda de peliculas

--- src/app/main-view/search/search.component.ts
+++ src/app/main-view/search/search.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { OmdbService } from '../../services/omdb.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+  query: string = '';
+  movies: any[] = [];
 
-  constructor() { }
+  constructor(private omdbService: OmdbService) { }
 
-  ngOnInit(): void {
+  search(): void {
+    if (this.query.trim()) {
+      this.omdbService.searchMovies(this.query).subscribe((response) => {
+        this.movies = response.Search;
+      });
+    }
   }
-
 }
