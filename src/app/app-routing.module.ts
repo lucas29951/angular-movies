8d3dfeb071626c7b16249561bd6dbe073aa01936
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-15 21:54:53 -0300

    Ruta inicial configurada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './main-view/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SearchComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
