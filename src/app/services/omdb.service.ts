5376ba1215f16bdeee74a030c91def4381c3ea38
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-15 21:59:49 -0300

    Actualizacion de la apiKey - tomada de las variables de entorno

--- src/app/services/omdb.service.ts
+++ src/app/services/omdb.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OmdbService {
-  private apiKey: string = '';
+  private apiKey: string = environment.apiKey;
   private baseUrl: string = 'http://www.omdbapi.com/';
 
   constructor(private http: HttpClient) { }
