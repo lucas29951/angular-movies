b5ac627b06f60cda786522156683093496295236
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-16 15:39:33 -0300

    Logica de la vista de detalle implementada

--- src/app/detail-view/movie-detail/movie-detail.component.ts
+++ src/app/detail-view/movie-detail/movie-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OmdbService } from '../../services/omdb.service';
 
 @Component({
   selector: 'app-movie-detail',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-detail.component.css']
 })
 export class MovieDetailComponent implements OnInit {
+  movie: any;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private omdbService: OmdbService
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.omdbService.getMovieDetails(id).subscribe((response) => {
+        this.movie = response;
+      });
+    }
   }
 
 }
