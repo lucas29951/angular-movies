b6bf25d30d83033b4b724aeec5185685f3c09ad3
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-16 15:21:43 -0300

    Ruta agregada para la vista detalle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SearchComponent } from './main-view/search/search.component';
+import { MovieDetailComponent } from './detail-view/movie-detail/movie-detail.component';
 
 const routes: Routes = [
   { path: '', component: SearchComponent },
+  { path: 'movie/:id', component: MovieDetailComponent },
 ];
 
 @NgModule({
