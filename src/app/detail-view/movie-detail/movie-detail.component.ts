2708473b16b634bb59a9e038f979a487dab9d5a2
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-17 16:33:07 -0300

    Logica implementada para agregar peliculas favoritas

--- src/app/detail-view/movie-detail/movie-detail.component.ts
+++ src/app/detail-view/movie-detail/movie-detail.component.ts
@@ -10,6 +10,7 @@ import { OmdbService } from '../../services/omdb.service';
 })
 export class MovieDetailComponent implements OnInit {
   movie: any;
+  isFavorite: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -22,6 +23,7 @@ export class MovieDetailComponent implements OnInit {
     if (id) {
       this.omdbService.getMovieDetails(id).subscribe((response) => {
         this.movie = response;
+        this.checkIfFavorite();
       });
     }
   }
@@ -30,4 +32,30 @@ export class MovieDetailComponent implements OnInit {
     this.location.back();
   }
 
+  checkIfFavorite(): void {
+    const favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+    this.isFavorite = favorites.some((fav: any) => fav.imdbID === this.movie.imdbID);
+  }
+
+  toggleFavorite(): void {
+    let favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+
+    if (this.isFavorite) {
+      favorites = favorites.filter((fav: any) => fav.imdbID !== this.movie.imdbID);
+    } else {
+      if (favorites.length < 15) {
+        favorites.push({
+          imdbID: this.movie.imdbID,
+          Title: this.movie.Title,
+          Poster: this.movie.Poster,
+          Year: this.movie.Year
+        });
+      } else {
+        alert('You can only store up to 15 favorite movies.');
+      }
+    }
+
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+    this.isFavorite = !this.isFavorite;
+  }
 }
