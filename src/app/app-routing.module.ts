56fc000489bf7190b484c03218db10895291cf18
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-17 17:07:31 -0300

    Ruta de favoritos agregada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SearchComponent } from './main-view/search/search.component';
 import { MovieDetailComponent } from './detail-view/movie-detail/movie-detail.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 const routes: Routes = [
   { path: '', component: SearchComponent },
   { path: 'movie/:id', component: MovieDetailComponent },
+  { path: 'favorites', component: FavoritesComponent },
 ];
 
 @NgModule({
