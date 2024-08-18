6464a169c686d1390f8ebc7098f95cf621bd975c
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-08-18 14:59:23 -0300

    Implementacion de carga inicial de la pagina

--- src/app/main-view/search/search.component.ts
+++ src/app/main-view/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { OmdbService } from '../../services/omdb.service';
 
 @Component({
@@ -6,12 +6,16 @@ import { OmdbService } from '../../services/omdb.service';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
   query: string = '';
   movies: any[] = [];
 
   constructor(private omdbService: OmdbService) { }
 
+  ngOnInit(): void {
+      
+  }
+  
   search(): void {
     if (this.query.trim()) {
       this.omdbService.searchMovies(this.query).subscribe((response) => {
